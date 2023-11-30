3991e78b66e2da8b144101708aee98fb7f7e0fa7
liqngliz <liqngliz@gmail.com>
2023-11-30 05:24:48 +0100

    sepolia

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./utils/setup');
 const { address, availableNetworks, provider, signer } = require('./utils/evm');
 const { ethers } = require('ethers');
+const Token = require('./Token.json');
 
 app.get('/', (req, res) =>
   res.status(200).json({
@@ -11,14 +12,21 @@ app.get('/', (req, res) =>
 //contract location 0xCdc46d1e16c2Eb5832f3B69e0f8eb6993FdAF684
 //network sepolia
 
-const balance = async (network) => {
-  const result = await provider(network).getBalance('0xc2f2F052eA0fd9FaB733c77A146905d25801d90A');
+const balance = async (network, address) => {
+  const result = await provider(network).getBalance(address);
   return ethers.utils.formatEther(result);
 };
 
-const GetHashOf = async (address, network, contract) => 
+const GetHashOf = async (address, network) => 
 {
-  
+  const provider = await provider(network);
+  const token = new ethers.Contract(
+    Token.address,
+    Token.abi,
+    address
+  );
+  let chainHash = await token.HashOf(address);  
+  return chainHash;
 }
 
 app
@@ -26,9 +34,11 @@ app
   .get(async (req, res) => {
     try {
       const { network } = req.params;
-      const value = await balance(network);
+      const value = await balance(network, '0xCdc46d1e16c2Eb5832f3B69e0f8eb6993FdAF684');
+      const hash = GetHashOf(network, '0xCdc46d1e16c2Eb5832f3B69e0f8eb6993FdAF684');
       res.status(200).json({
         message: 'My balance is ' + value + ' ethers',
+        hash : hash
       });
     } catch (e) {
       const error = e.toString();
